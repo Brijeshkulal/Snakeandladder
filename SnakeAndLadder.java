2a2e7402052d55fd04bc47f28d8552444f2cc57b
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-01 12:39:23 +0530

    Random dice value is generated

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,12 @@
 public class SnakeAndLadder
 {
-	public static void main( String[] args )
+	public static void main(String[] args)
 	{
-        System.out.println( "Welcome To Snake And Ladder Simulator" );
+		System.out.println( "Welcome To Snake And Ladder Simulator" );
 		int startPosition = 0;
 		int endPosition = 100;
-		System.out.println("stating position of player is" + startPosition);
+		System.out.println("stating position of player is " + startPosition);
+		int diceValue = (int) Math.floor((Math.random() * 10) %6) + 1;
+		System.out.println("dice value is " + diceValue);
 	}
 }
