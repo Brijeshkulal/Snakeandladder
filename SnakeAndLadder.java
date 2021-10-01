56b77943b8fba4d7c8729286a40bb52fbedde901
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-01 18:41:04 +0530

    Added number of times dice was rooled to win the game

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,11 +5,11 @@ public class SnakeAndLadder
 		System.out.println( "Welcome To Snake And Ladder Simulator" );
 		int startPosition = 0;
 		int finalPosition = 100;
+		int count_dice_roll = 0;
 		System.out.println("stating position of player is " + startPosition);
 		while(startPosition != finalPosition)
 		{
 			int diceValue = (int) Math.floor((Math.random() * 10) %6) + 1;
-			System.out.println("dice value is " + diceValue);
 			int checkOption = (int) Math.floor((Math.random() * 10) %3);
 			switch(checkOption){
 				case 1:
@@ -21,7 +21,7 @@ public class SnakeAndLadder
 				default:
 				startPosition -= diceValue;
 				}
-			System.out.println("option value is " +checkOption);
+			System.out.println("dice value : " + diceValue + " || option value : " +checkOption);
 			if(startPosition < 0)
 			{
 				startPosition = 0;
@@ -30,7 +30,9 @@ public class SnakeAndLadder
 			{
 				startPosition -= diceValue;
 			}
-			System.out.println("Position of the player is at " +startPosition);
+			System.out.println("Position of the player : " +startPosition);
+			count_dice_roll++;
 		}	
+		System.out.println("Number of times the dice was rolled to win the game is " +count_dice_roll);
 	}
 }
