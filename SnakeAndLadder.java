391090a062c56021b767002241e4237c8f5a5028
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-01 11:27:10 +0530

    iniatilize the starting position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,10 @@
 public class SnakeAndLadder
 {
-    public static void main( String[] args )
-
-    {
+	public static void main( String[] args )
+	{
         System.out.println( "Welcome To Snake And Ladder Simulator" );
-    }
+		int startPosition = 0;
+		int endPosition = 100;
+		System.out.println("stating position of player is" + startPosition);
+	}
 }
