fedf49f8b8adea580cdc8f3a713e4557e24a21c9
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-01 13:28:35 +0530

    Added option values to play

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,5 +8,19 @@ public class SnakeAndLadder
 		System.out.println("stating position of player is " + startPosition);
 		int diceValue = (int) Math.floor((Math.random() * 10) %6) + 1;
 		System.out.println("dice value is " + diceValue);
+		int checkOption = (int) Math.floor((Math.random() * 10) %3);
+		switch(checkOption){
+			case 1:
+			startPosition += 0;
+			break;
+			case 2:
+			startPosition += diceValue;
+			break;
+			default:
+			startPosition -= diceValue;
+		}
+		System.out.println("option value is " +checkOption);
+		System.out.println("Position of the player is at " +startPosition);
+			
 	}
 }
